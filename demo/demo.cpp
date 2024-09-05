a10ee5803ebf02cb6f280a6c31d2fb8e97bad92d
Abhinay <abhinaykmr123@gmail.com>
2024-09-06 01:57:01 +0530

    commit 1

---
+++ demo/demo.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main(){
+    int n ;
+    int sum =0  ;
+    cout << "Enter Number" << endl ;
+    cin >> n ;
+
+    for(int i=0 ;i<n ;i++){
+        if(i%3 ==0 )sum += i ;
+    }
+    cout << sum << endl ;
+}
