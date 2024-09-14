86a720cd0b8adcf5c0b1dbf5e6fe0d3911d3571c
Abhinay <abhinaykmr123@gmail.com>
2024-09-14 23:18:02 +0530

    bubbleshort

---
+++ singlenumvec.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void singleNum(int ans){
+    int ans=0;
+    int num;
+    for(int val : num){
+        ans^=val;
+    }
+    return ans;
+}
+
+int main(){
+    vector<int>num={4 ,8 ,4 ,9 ,8 , 1 ,9};
+    singleNum(ans,num)
+
+
+}
