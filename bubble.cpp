8b487cf2f950be261f04ac021fe7d1b899171ee4
Abhinay <abhinaykmr123@gmail.com>
2024-09-18 23:52:09 +0530

    selection

---
+++ bubble.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+// Function to swap two elements
+void swap(int *xp, int *yp) {
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// Function to perform selection sort
+void selectionSort(int arr[], int n) {
+    int i, j, min_idx;
+
+    // One by one move the boundary of the unsorted subarray
+    for (i = 0; i < n-1; i++) {
+        // Find the minimum element in the unsorted array
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+
+        // Swap the found minimum element with the first element
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// Main function to test the selection sort
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    selectionSort(arr, n);
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+    return 0;
+}
